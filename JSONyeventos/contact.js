2541055f2b6b0ef5b993ce6ccb42959621df0668
NicoSM89 <107883382+NicoSM89@users.noreply.github.com>
2022-09-21 18:44:59 -0300

    Pre_entrega_2
    agregando json

---
+++ JSONyeventos/contact.js
@@ -0,0 +1,39 @@
+var inputs = document.getElementsByClassName('formulario__input');
+for (var i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('keyup', function () {
+        if (this.value.length >= 1) {
+            this.nextElementSibling.classList.add('fijar');
+        } else {
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    });
+}
+
+/* function validateEmail() {
+    
+    var emailField = document.getElementById('email');
+
+    var validEmail = /^\w+([.-_+]?\w+)@\w+([.-]?\w+)(\.\w{2,10})+$/;
+    
+    if (validEmail.test(emailField.value)) {
+        return true;
+    } else {
+        alert('Email invalido')
+        return false;
+    }
+} */
+
+const titulo = document.getElementsByClassName('formulario__input')
+const formulario = document.getElementById('formInicial')
+
+formulario.onsubmit = (event) => {
+    event.preventDefault()
+
+    for (const input of event.target.children) {
+        const obj = {}
+        obj['tipo'] = input.name
+        obj['valor'] = input.value
+        console.log(obj)
+        localStorage.setItem(obj.tipo,JSON.stringify,obj.valor)
+    }
+}
